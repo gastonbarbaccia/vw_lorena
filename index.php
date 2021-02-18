982bb6120356ae6a5d26576194a1e24b5961eb79
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2021-02-18 10:45:02 -0300

    Se modifica el mensaje de whatsapp

--- index.php
+++ index.php
@@ -681,7 +681,7 @@ banner -->
     <!--=================================
  contact whatsapp -->
 
-    <a href="https://wa.me/541138497740?text=Me%20gustaría%20saber%20el%20precio%20del%20coche" class="whatsapp" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
+    <a href="https://wa.me/541138497740?text=Hola%20me%20podrias%20mandar%20mas%20información?" class="whatsapp" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
 
     <!--=================================
  get-quote -->
