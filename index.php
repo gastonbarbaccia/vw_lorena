fea75ed373d4d2991d9e5470d9c4891101733653
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2021-03-19 20:17:04 -0300

    Se quita el mapa del footer

--- index.php
+++ index.php
@@ -700,11 +700,13 @@ banner -->
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-12">
+                <!--
                     <div class="contact-form">
                         <h5>Find Dealer</h5>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3224.017231421863!2d-79.43780268425046!3d36.09306798010035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88532bae09664ccb%3A0xaa6b8f98d3fb8135!2s220+E+Front+St%2C+Burlington%2C+NC+27215%2C+USA!5e0!3m2!1sen!2sin!4v1475045272926" style="width: 100%; height: 324px; border:0" allowfullscreen></iframe>
                     </div>
                 </div>
+                -->
             </div>
         </div>
         </div>
